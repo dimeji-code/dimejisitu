0ac733bf71c73312f0953a9cc9da4f5be8677a31
dimeji-code <dimeji.situ@yahoo.com>
2022-07-02 21:55:59 -0400

    profile slider

---
+++ app.js
@@ -0,0 +1,25 @@
+$('.transitioner').click(function(){
+    //do something
+    $('.slider').css('background-color',"#b54c40")
+    $('.slider').css('left',"0%")
+
+  });
+
+  $('.exit').click(function(){
+    //do something
+    $('.slider').css('background-color',"#b54c40")
+    $('.slider').css('left',"100%")
+
+  });
+
+// $('.transitioner').hover(
+// function(){
+// //do something
+// $('.slider').css('background-color',"#cfc")
+// $('.slider').css('left',"98%")
+// }, function(){
+// //do something
+// $('.slider').css('background-color',"#ffc")
+// $('.slider').css('left',"100%")
+// }
+// );
